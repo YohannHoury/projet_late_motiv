3715def9c53479ed63d83ea3ab6a1b2227672706
YohannHoury <yohannhoury@ip-172-31-22-229.eu-west-3.compute.internal>
2022-08-12 12:21:34 +0000

    important modifcations files and folders add files.phtml and css

---
+++ config/DataBase.php
@@ -0,0 +1,7 @@
+<?php
+$pdo = new PDO ("mysql:host=db.3wa.io;port=3306;dbname=yohannhoury_late_motiv_app;charset=utf8",
+                "yohannhoury","40347cba18f8c6405e376f555da73be9");
+$result = $pdo->prepare('SELECT * FROM pages');
+$result->execute();
+$sql = $result->fetch(PDO::FETCH_ASSOC);
+return $sql;
