66b704ddbe512bf9e8a57869795bd07deced8566
YohannHoury <106591031+YohannHoury@users.noreply.github.com>
2022-09-18 17:12:08 +0200

    Delete autoload.php

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('models/Router.php');
-
-spl_autoload_register(function($className) {
-    require_once lcfirst(str_replace('\\','/', $className)) . '.php';
-});
-
-$router = new Router();
-?>
